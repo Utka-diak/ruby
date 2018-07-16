4396d2d7283e5f1dc827d935bb56969110a8d388
uliana_diak <ulianadiak@mail.ru>
2018-07-16 14:54:35 +0400

    Finished triangle_area

---
+++ lesson_1/triangle_area.rb
@@ -0,0 +1,8 @@
+print "State the length of the base of the triangle."
+a = gets.chomp
+
+print "State the height of the triangle."
+h = gets.chomp
+
+area = ( 1 / 2.0 ) * a.to_i * h.to_i 
+puts "The area of the triangle is #{area}."
