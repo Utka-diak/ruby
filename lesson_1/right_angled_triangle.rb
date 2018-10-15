836a0a22954059dbaa70c13286e0a63824259318
Uls <ulianadiak@mail.ru>
2018-10-15 17:29:13 +0300

    further correction of right_angled_triangle.rb

--- lesson_1/right_angled_triangle.rb
+++ lesson_1/right_angled_triangle.rb
@@ -13,15 +13,15 @@ elsif b > a && b > c
   hypotenuse = b
   length_1 = a
   length_2 = c
-elsif hypotenuse = c
+else
+  hypotenuse = c
   length_1 = a
   length_2 = b
-
+end
 if hypotenuse**2 == length_1**2 + length_2**2 && length_1 == length_2
-    puts "The triangle is right angled and isosceles!"
+  puts "The triangle is right angled and isosceles!"
 elsif hypotenuse**2 == length_1**2 + length_2**2
   puts "The triangle is right angled!"
 elsif hypotenuse == length_1 && length_1 == length_2
   puts "The triangle is not right angled but equilateral."
-end
 end
