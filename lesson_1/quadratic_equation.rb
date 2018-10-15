3cb4aa7234a57d49313a0457d1b5fe623a48cae8
Uls <ulianadiak@mail.ru>
2018-10-15 14:53:07 +0300

    corrected quadratic_equation.rb

--- lesson_1/quadratic_equation.rb
+++ lesson_1/quadratic_equation.rb
@@ -1,18 +1,18 @@
 puts "Using the following formula (for quadratic equations),
 ax^2 + bx + c = 0, state the value of the ccoefficient a."
-a = gets.to_i
+a = gets.to_f
 puts "State the value of coefficient b."
-b = gets.to_i
+b = gets.to_f
 puts "State the value of coefficient c."
-c = gets.to_i
+c = gets.to_f
 
-D = ( b**2 ) - ( 4 * a * c )
-x1 = ( - b + Math.sqrt(D) ) / ( 2 * a )
-x2 = ( b - Math.sqrt(D) ) / ( 2 * a )
-if D < 0
-  puts "discriminant = #{D}."
-elsif D == 0
-  puts "discriminant = #{D}, x = #{x1}"
-elsif D > 0
-  puts "discriminant = #{D}, x = #{x1},#{x2}"
+d = b**2 - 4 * a * c
+if d < 0
+  puts "discriminant = #{d}."
+elsif d > 0
+  x1 = ( - b + Math.sqrt(d) ) / ( 2 * a )
+  x2 = ( - b - Math.sqrt(d) ) / ( 2 * a )
+  puts "discriminant = #{d}, x = #{x1},#{x2}"
+elsif
+  puts "discriminant = #{d}, x = #{x1}"
 end
