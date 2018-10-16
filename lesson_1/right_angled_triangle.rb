813644e4c181da66fffdf41ae392537fb5a0a515
Uls <ulianadiak@mail.ru>
2018-10-16 10:06:44 +0300

    correction with help of right_angled_triangle.rb

--- lesson_1/right_angled_triangle.rb
+++ lesson_1/right_angled_triangle.rb
@@ -5,23 +5,16 @@ b = gets.to_f
 print "State the length of the remaining side."
 c = gets.to_f
 
-if a > b && a > c
-  hypotenuse = a
-  length_1 = b
-  length_2 = c
-elsif b > a && b > c
-  hypotenuse = b
-  length_1 = a
-  length_2 = c
-else
-  hypotenuse = c
-  length_1 = a
-  length_2 = b
-end
-if hypotenuse**2 == length_1**2 + length_2**2 && length_1 == length_2
+sides = [a, b, c]
+sides.sort!
+condition_1 = sides[2]**2 == sides[0]**2 + sides[1]**2
+condition_2 = sides[0] == sides [1]
+condition_3 = a == b && b == c
+
+if condition_1 && condition_2
   puts "The triangle is right angled and isosceles!"
-elsif hypotenuse**2 == length_1**2 + length_2**2
+elsif condition_1
   puts "The triangle is right angled!"
-elsif hypotenuse == length_1 && length_1 == length_2
+elsif condition_3
   puts "The triangle is not right angled but equilateral."
 end
