c3600f53a01059b340f02973a8ae2b971139dafa
Uls <ulianadiak@mail.ru>
2019-01-07 14:10:35 +0300

    corrected basket_sum.rb

--- lesson_2/basket_sum.rb
+++ lesson_2/basket_sum.rb
@@ -1,22 +1,22 @@
 purchases_hash = {}
 product = ""
-while product != "stop"
-puts "What have you bought?"
+loop do
+  puts "What have you bought?"
   product = gets.chomp
-  if product != "stop"
-    purchases_hash[product] = {}
-    if product != "stop"
-      puts "How much was it?"
-      price_value = gets.to_i
-      purchases_hash[product][:price] = price_value
-      puts "In what quantity?"
-      quantity_value = gets.to_i
-      purchases_hash[product][:quantity] = quantity_value
-    end
-  end
+  break if product == "stop"
+  purchases_hash[product] = {}
+  puts "How much was it?"
+  price_value = gets.to_i
+  purchases_hash[product][:price] = price_value
+  puts "In what quantity?"
+  quantity_value = gets.to_i
+  purchases_hash[product][:quantity] = quantity_value
 end
 puts purchases_hash
-purchases_hash.each { |key, value| puts "#{key} total price is #{value[:price] * value[:quantity]}" }
 purchases_sum = 0
-purchases_hash.each { |key, value| purchases_sum += value[:price] * value[:quantity] }
+purchases_hash.each do |item, info|
+  price_per_item = info[:price] * info[:quantity]
+puts "#{item} total price is #{price_per_item}"
+purchases_sum += price_per_item
+end
 puts purchases_sum
