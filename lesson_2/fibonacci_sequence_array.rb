02bad868f2f8477242996c18a3a012ea52d2c22b
Uls <ulianadiak@mail.ru>
2019-01-07 14:54:39 +0300

    thank you next.. corrected

--- lesson_2/fibonacci_sequence_array.rb
+++ lesson_2/fibonacci_sequence_array.rb
@@ -1,7 +1,7 @@
-sequence = [0,1]
-calculation = 1
-while calculation < 100
-  sequence.push(calculation)
-  calculation = sequence[-1] + sequence[-2]
+sequence = [0, 1]
+next_number = 1
+while next_number < 100
+  sequence.push(next_number)
+  next_number = sequence[-1] + sequence[-2]
 end
 puts sequence
