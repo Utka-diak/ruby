c91de921a1c7c9560109fcb2ba87a8c7e20829c2
Uls <ulianadiak@mail.ru>
2019-01-07 16:14:36 +0300

    corrected months_days.rb

--- lesson_2/months_days.rb
+++ lesson_2/months_days.rb
@@ -1,4 +1,17 @@
-months = { "January" => 31, "February" => 28, "March" => 31, "April" => 30,
-   "May" => 31, "June" => 30, "July" => 31, "August" => 31,
-   "September" => 30, "October" => 31, "November" => 30, "December" => 31 }
-puts months.select { |k,v| v == 30 }
+months = {
+  January: 31,
+  February: 28,
+  March: 31,
+  April: 30,
+  May: 31,
+  June: 30,
+  July: 31,
+  August: 31,
+  September: 30,
+  October: 31,
+  November: 30,
+  December: 31
+}
+months.each do |month, days|
+  puts month if days == 30
+end
