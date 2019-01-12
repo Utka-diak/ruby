3d998478e5616b16465652aa436a5644e3797564
Uls <ulianadiak@mail.ru>
2019-01-12 16:19:22 +0300

    added .reduce

--- lesson_2/date_index.rb
+++ lesson_2/date_index.rb
@@ -9,5 +9,8 @@ if year % 4 == 0 && year % 100 != 0 || year % 400 == 0
   common_year[1] += 1
 end
 arr_index = month - 1
-index = common_year[0...arr_index].sum + day
+if arr_index == 0
+  index = day
+  else index = common_year.first(arr_index).reduce(:+) + day
+end
 puts index
